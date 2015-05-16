071718d1cddd91468ffccc759d49d77ef5762281
benjamin tillett <benjamin.tillett@gmail.com>
2015-05-16 15:02:02 +0100

    finish http collect

---
+++ httpCollect.js
@@ -0,0 +1,25 @@
+var http 	= require('http');
+var url 	= process.argv[2];
+
+const BufferList = require('bl');
+var bl = new BufferList();
+
+
+
+
+
+
+http.get(url,function(response){
+	response.setEncoding('utf8');
+	
+	response.on('data',function(data){
+		bl.append(data);		
+	});		
+	
+	response.on('error',console.error);
+	
+	response.on('end',function(string){
+		console.log(bl.length);
+		console.log(bl.toString());
+	});
+});
