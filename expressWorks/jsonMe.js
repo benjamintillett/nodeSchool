1e0b77f1f90b3069f47e950532aa468306b65892
benjamin tillett <benjamin.tillett@gmail.com>
2015-05-24 14:22:32 +0100

    finish jsonMe

---
+++ expressWorks/jsonMe.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var fs	= require('fs');
+
+
+app.get('/books',function(req,res){
+	fs.readFile(process.argv[3],'utf8',function(err,data){
+		if(err){
+			throw err; 
+		}
+		else {
+			object = JSON.parse(data);
+			res.send(object);
+		}
+	});
+});
+
+app.listen(process.argv[2]); 
